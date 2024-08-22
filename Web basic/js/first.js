4553c676446250e5b18e2015ca211afa2090abee
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-08-22 15:45:14 +0530

    Add Switch Statement

--- Web basic/js/first.js
+++ Web basic/js/first.js
@@ -192,7 +192,34 @@ if (hour < 10 ){
 
 console.log( greeting );
 
+//JS Switch Statement
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+         day = "Wednesday";
+         break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case  6:
+        day = "Saturday";
+    default:
+        text = "Looking forward to the Weekend";
+}
 
+console.log( "Today is", + day );
 
 
 
