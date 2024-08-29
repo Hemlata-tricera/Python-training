8cd7f591010d327f279cffba4cba4e08fa3f0a0b
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-08-29 16:11:14 +0530

    Add string escape character

--- Python/string.py
+++ Python/string.py
@@ -79,3 +79,25 @@ print(txt)
 txt = f"The price is {10 * 20} dollars"  # Here placeholder contain math python code(math operation)
 print(txt)
 
+# Escape Characters
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+txt = "For new line please insert \\n "
+print(txt)
+
+txt = "Hello\nWorld!"
+print(txt)
+txt = "Hello\tWorld!"
+print(txt)
+# This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt)
+# A backslash followed by three integers will result in a octal value
+txt = "\110\145\154\154\157"
+print(txt)
+# A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
+
+
