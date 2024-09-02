ea53d209dfa6a030bf233b11491baf0062361743
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-02 15:08:41 +0530

    Add For loop

--- Python/conditions.py
+++ Python/conditions.py
@@ -106,3 +106,66 @@ else:
     print("i is no longer leas than 6")  # Print a message once the condition is false:
 
 
+
+# FOR LOOP -A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# BREAK Statement- With the break statement we can stop the loop before it has looped through all the items
+# Example- Exit the loop when x is "banana":
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+# Example-2 Exit the loop when x is "banana", but this time the break comes before the print
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+# The continue Statement- With the continue statement we can stop the current iteration of the loop, and continue with the next:
+
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+# The range() Function - The range() function returns a sequence of numbers, starting from 0 by default,
+# and increments by 1 (by default), and ends at a specified number.
+
+for x in range(6):   # Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
+    print(x)
+
+
+# it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6)
+for x in range(2, 6):
+    print(x)
+
+# it is possible to specify the increment value by adding a third parameter: range(2, 30, 3)
+for x in range(2, 20, 2):
+    print(x)
+
+
+# Else in For Loop - The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+for x in range(6):
+    print(x)
+else:
+    print("Finally Finished")
+
+
+# Nested Loops-A nested loop is a loop inside a loop.
+# The "inner loop" will be executed one time for each iteration of the "outer loop"
+greetings = ["Good Morning", "Good Afternoon", "Good Evening"]
+users = ["Yogesh!", "Somesh!", "Rakesh!" ]
+
+for x in greetings:
+    for y in users:
+        print(x, y)
+
+
+# The pass Statement
+for x in [0, 1, 2]:
+  pass
+
+
