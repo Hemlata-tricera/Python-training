951734bf5f9acff30564ca22d037dc2ad3224ef6
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-04 11:28:48 +0530

    Add function defination

---
+++ Python/function_class.py
@@ -0,0 +1,9 @@
+# Creating a Function - In Python a function is defined using the def keyword
+def my_function():
+    print("Hello, I am function")
+
+my_function()
+
+# function with arguments
+
+
