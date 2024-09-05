57e292787e3bd15771db88fcf013350538ab08d8
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-05 07:20:48 +0530

    Add function calling

--- Python/function.py
+++ Python/function.py
@@ -3,3 +3,11 @@
 
 def my_function():
   print("Hello from a function")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis:
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
