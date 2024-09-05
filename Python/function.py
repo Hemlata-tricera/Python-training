f120af015c50d73a6c18e0914b981a64f2081d70
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-05 07:46:43 +0530

    Add arbitary arguments, keyword arguments, arbitary keyword and default parameter value

--- Python/function.py
+++ Python/function.py
@@ -20,3 +20,48 @@ full_name("Jayesh")
 full_name("Suresh")
 full_name("Mangesh")
 
+# Numbers of argument
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")   # This function expects 2 arguments, and gets 2 arguments:
+
+
+# Arbitrary Arguments, *args
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+# Keyword Arguments
+# You can also send arguments with the key = value syntax.
+# This way the order of the arguments does not matter
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Suresh")
+
+
+# Arbitrary Keyword Arguments, **kwargs
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+
+# Default Parameter Value
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+# The pass Statement
+# function definitions cannot be empty, but if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
+def myfunction():
+  pass
