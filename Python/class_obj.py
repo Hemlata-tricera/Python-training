f8064d21dc1016ca9dabb0061a55c220c651e6a8
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-08 16:46:58 +0530

    Add the self parameter in class

--- Python/class_obj.py
+++ Python/class_obj.py
@@ -8,11 +8,11 @@ print(p1.x)
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age):      # The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.
         self.name = name
         self.age = age
 
-    def __str__(self):
+    def __str__(self):                                    # Str function
         return f"{self.name}({self.age})"
 
 
@@ -21,3 +21,18 @@ p2 = Person("Amish", 45)
 print(p2.name)
 print(p2.age)
 print(p2)
+
+# The self Parameter
+class Person:
+    def __init__(myobject, name, age):
+        myobject.name = name
+        myobject.age = age
+
+    def myfun(abc):
+        print("Hello my name is " + abc.name)
+
+
+p1 = Person("avinash", 35)
+p1.myfun()
+
+
