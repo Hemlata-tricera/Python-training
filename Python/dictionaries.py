839a6b2b826f3a4623394f5ca265fda048ddc2a4
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-12 17:21:36 +0530

    Add dictionary add,remove,update,delete and clear operations

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -16,6 +16,46 @@ print(len(thisdict))
 print(type(thisdict))
 
 # The dict() Constructor
-thisdict = dict(name = "John", age = 36, country = "Norway")
+thisdic = dict(name = "John", age = 36, country = "Norway")
+print(thisdic)
+
+# Accessing Items
+x = thisdict["model"]
+print(x)
+
+# OR
+
+x = thisdict.get("model")
+print(x)
+
+#Get Keys
+
+x = thisdict.keys()
+print(x)
+
+# Get Values
+x = thisdict.values()
+print(x)
+
+# Update Dictionary
+thisdict.update({"year": 2020})
 print(thisdict)
 
+# Adding Items
+thisdict["color"] = "red"
+print(thisdict)
+
+# Removing Items
+thisdict.pop("model")  # The pop() method removes the item with the specified key name
+print(thisdict)
+
+thisdict.popitem()  # The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead)
+print(thisdict)
+
+# The del keyword removes the item with the specified key name
+del thisdict["year"]
+print(thisdict)
+
+# The clear() method empties the dictionary
+thisdict.clear()
+print(thisdict)
