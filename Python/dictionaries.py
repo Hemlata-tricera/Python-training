b52e32e951730a5003716a7732b6d8f2430c08a6
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-12 16:01:54 +0530

    Add dictionary length, type and dict() constructor

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -9,4 +9,13 @@ print(thisdict)
 # Dictionary Items
 print(thisdict["brand"])
 
+# Dictionary Length
+print(len(thisdict))
+
+# Dictionary Items - Data Types
+print(type(thisdict))
+
+# The dict() Constructor
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
 
