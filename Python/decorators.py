d7a6fcd9e13a997d507ac824d692cb2855e60466
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-15 13:07:21 +0530

    Add initial commit for decorators

--- Python/decorators.py
+++ Python/decorators.py
@@ -1,1 +1,10 @@
-# function can take another function as an argumen
+# function can take another function as an argument
+
+def increment(x):
+    return x + 1
+
+def operate(func, x):
+    result = func(x)
+    return result
+
+print(operate(increment, 3))
