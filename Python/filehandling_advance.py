27a35e33dd9b1dcb67d0a1b81d91ea0abbf4a004
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-09-19 12:28:43 +0530

    Add file handling advance concept

--- Python/filehandling_advance.py
+++ Python/filehandling_advance.py
@@ -1,19 +1,25 @@
-f = open("myfile.txt")
-print(f.read())
-f.close()
+# f = open("myfile.txt")
+# print(f.read())
+# f.close()
 
 # f = open("myfile.txt", 'a')
 # data = f.write("This is new added line 4")
 # print(data)
 # f.close()
 
-# f = open("myfile.txt", 'a+')
-# data = f.write("This is new added line 5")
-# # print(data)
+# It will append string, It can be perform on existing or non existing file(It will create new file).
+f = open("myfile123.txt", 'a+')
+data = f.write("This is a+ mode2")
+# print(data)
+f.close()
+
+
+# # Perform on existing file only
+# f =open("myfile.txt", 'r+')
+# f.write("This is r+ mode 2")
 # f.close()
 
-
-
-f =open("myfile.txt", 'r+')
-f.write("This is r+ mode")
-f.close()
+# It will overide content, It can be perform on existing or non existing file(It will create new file).
+# f =open("myfile.txt", 'w+')
+# f.write("This is w+ mode")
+# f.close()
