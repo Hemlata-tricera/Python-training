9b14ada0266bcc4464acf31f87c3fefc245d7974
Hemlata-tricera <hemlata.raghuvanshi@tricera.tech>
2024-11-20 16:51:08 +0530

    Add basic sql commands

---
+++ SQL/basic_commands.sql
@@ -0,0 +1,9 @@
+/* SQL Shell(PSQL) Commands-
+\list - List available databases.
+\c dbname - Switch connection to a new database.
+\dt – List available table.
+\d table_name - Describe a table such as a
+column, type, modifiers of columns, etc.
+\dv - List available views in the current
+database
+*/
